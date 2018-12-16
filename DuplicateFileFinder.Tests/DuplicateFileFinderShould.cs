3e1d701e46d83cb2b11cfcd929da873d24c72864
Scott Edwards <scotteza@gmail.com>
2018-12-16 17:56:34 +0000

    Refactor unit tests

--- DuplicateFileFinder.Tests/DuplicateFileFinderShould.cs
+++ DuplicateFileFinder.Tests/DuplicateFileFinderShould.cs
@@ -7,25 +7,34 @@ namespace DuplicateFileFinder.Tests
     [TestFixture]
     internal class DuplicateFileFinderShould
     {
+        private Mock<IDirectoryParser> _directoryParser;
+        private DuplicateFileFinder _duplicateFileFinder;
+        private string _rootDirectory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _rootDirectory = ".";
+            _directoryParser = new Mock<IDirectoryParser>();
+            _duplicateFileFinder = new DuplicateFileFinder(_directoryParser.Object);
+        }
+
         [Test]
         public void Find_Duplicate_File_Using_A_DirectoryParser()
         {
-            var rootDirectory = ".";
             var directoryData = new List<DirectoryData>
             {
                 new DirectoryData("Directory 1", @"c:\Directory 1"),
                 new DirectoryData("Directory 2", @"c:\Directory 2"),
                 new DirectoryData("Directory 3", @"c:\Directory 3")
             };
-            var directoryParser = new Mock<IDirectoryParser>();
-            directoryParser.Setup(dp => dp.FindAllDirectories(rootDirectory, IncludeRootDirectoryInResults.Yes))
+            _directoryParser.Setup(dp => dp.FindAllDirectories(_rootDirectory, IncludeRootDirectoryInResults.Yes))
                 .Returns(directoryData);
-            var duplicateFileFinder = new DuplicateFileFinder(directoryParser.Object);
+            
+            var duplicates = _duplicateFileFinder.GetDuplicates(_rootDirectory);
 
-            var duplicates = duplicateFileFinder.GetDuplicates(rootDirectory);
-
-            directoryParser.Verify(dp => dp.FindAllDirectories(rootDirectory, IncludeRootDirectoryInResults.Yes));
-            directoryParser.Verify(dp => dp.FindAllFiles(directoryData));
+            _directoryParser.Verify(dp => dp.FindAllDirectories(_rootDirectory, IncludeRootDirectoryInResults.Yes));
+            _directoryParser.Verify(dp => dp.FindAllFiles(directoryData));
         }
     }
 }
