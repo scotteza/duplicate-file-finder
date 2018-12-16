5b32bf652b5b1be415452f6d5c287924a59d06a6
Scott Edwards <scotteza@gmail.com>
2018-12-16 17:10:21 +0000

    Add console project

---
+++ DuplicateFileFinder.Console/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DuplicateFileFinder.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                WriteInvalidDirectoryMessage();
+                return;
+            }
+
+            var rootDirectory = args.First();
+
+            try
+            {
+                if (!Directory.Exists(rootDirectory))
+                {
+                    WriteInvalidDirectoryMessage();
+                    return;
+                }
+            }
+            catch
+            {
+                WriteInvalidDirectoryMessage();
+                return;
+            }
+
+            var directoryParser = new WindowsDirectoryParser();
+            var directories = directoryParser.FindAllDirectories(rootDirectory, IncludeRootDirectoryInResults.Yes);
+            var files = directoryParser.FindAllFiles(directories);
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        private static void WriteInvalidDirectoryMessage()
+        {
+            Console.WriteLine("Please pass in a valid directory as an argument.");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadLine();
+        }
+    }
+}
